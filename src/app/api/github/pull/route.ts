455e81e39d423bc2b94a5dc810e37c07e9a7e02f
Calen Cooper <127441048+CalenCoop@users.noreply.github.com>
2025-08-13 19:20:36 -0700

    Delete route.ts

--- src/app/api/github/pull/route.ts
+++
@@ -1,6 +0,0 @@
-// import { FetchGitHub } from "@/lib/github";
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function GET(request: NextRequest ){
-//  const repo = request.nextUrl.searchParams.get("repo")
-// }
